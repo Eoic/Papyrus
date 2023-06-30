1da3ecb74e91417fb44f97f1500663a92a116757
Eoic <karolis.strazdas@protonmail.com>
2023-06-30 23:53:07 +0300

    Reconfigure Firebase auth.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:client/providers/google_sign_in_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 
 import 'config/app_router.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +9,7 @@ import 'package:client/themes/color_schemes.g.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const Papyrus());
 }
 
--- client/lib/widgets/drawer_scaffold.dart
+++ client/lib/widgets/drawer_scaffold.dart
@@ -1,4 +1,3 @@
-import 'package:client/pages/stubPage.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:collection/collection.dart';
