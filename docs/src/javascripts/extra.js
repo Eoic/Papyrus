e7929aa7be8532b1d6a5dd6c9c52049c563d02f4
Eoic <karolis.strazdas@protonmail.com>
2026-03-03 13:06:30 +0200

    chore: move docs to separate repository

--- docs/src/javascripts/extra.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelectorAll('a[href^="http"]').forEach(function (link) {
-    if (!link.hostname.includes(window.location.hostname)) {
-      link.setAttribute('target', '_blank');
-      link.setAttribute('rel', 'noopener noreferrer');
-    }
-  });
-});
